3d62e345b35ac08fd5551066b4b215d263c000e5
yjw8459 <ryu01410@gmail.com>
2021-12-25 00:44:31 +0900

    Initial commit

---
+++ src/main/java/com/yjw/jwshop/JwshopApplication.java
@@ -0,0 +1,13 @@
+package com.yjw.jwshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yjw/jwshop/JwshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjw.jwshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
