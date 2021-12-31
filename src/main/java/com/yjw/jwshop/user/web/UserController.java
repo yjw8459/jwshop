4bb6f51b8b926261e339544582fbd1e2aa39bfc3
yjw8459 <ryu01410@gmail.com>
2021-12-31 23:30:08 +0900

    login - 1

--- src/main/java/com/yjw/jwshop/user/web/UserController.java
+++ src/main/java/com/yjw/jwshop/user/web/UserController.java
@@ -4,7 +4,6 @@ import com.yjw.jwshop.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,7 +14,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping(value = "/login")
-    public ModelAndView login(@RequestParam(value = "code") String code){
+    public ModelAndView login(@RequestParam(value = "code", required = false) String code){
         return new ModelAndView("/user/login");
     }
 
